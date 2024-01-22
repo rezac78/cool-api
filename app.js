d6c3230e7fca52a350c1f8d7dd4c6a09f24f7a43
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-01-22 17:55:11 +0330

    https://coollearning.netlify.app

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "development") {
 // Middleware setup
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://your-netlify-app.netlify.app', 
+  origin: 'https://coollearning.netlify.app', 
   credentials: true, 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type'],
