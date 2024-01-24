6c6aff73edc34969aff01cbcb6b989f047bdbffd
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-01-24 10:16:50 +0330

    local storage

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -2,14 +2,12 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 exports.protect = async (req, res, next) => {
   let token;
-  if (req.cookies && req.cookies.token) {
-    token = req.cookies.token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
   }
   if (!token) {
-    return res
-      .status(401)
-      .json({ success: false, message: "Not authorized to access this route" });
-  }
+    return res.status(401).json({ success: false, message: "Not authorized to access this route" });
+}
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id);
